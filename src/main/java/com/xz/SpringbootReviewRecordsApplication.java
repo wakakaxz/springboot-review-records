8d0a5d8e20c58a291379c41062aa26180c566eee
wakakaxz <wakakaxiaozheng@163.com>
2022-09-05 15:29:41 +0800

    first commit

---
+++ src/main/java/com/xz/SpringbootReviewRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.xz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReviewRecordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReviewRecordsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xz/SpringbootReviewRecordsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.xz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReviewRecordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("你好世界");
+    }
+
+}
